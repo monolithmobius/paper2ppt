fc0a02e70e11c0c83a3ec03a1f06404a119e25f1
Gabrinawu <hl_2020@qq.com>
2022-02-18 16:41:45 +0800

    summarize the text successfully and add this part into the pipeline

--- try.py
+++ try.py
@@ -5,16 +5,11 @@
 import zipfile
 # latex parser
 from TexSoup import TexSoup
-# latex parser to extract string of image name from tex
-from TexSoup.category import categorize
-from TexSoup.tokens import tokenize
-from TexSoup.reader import read_item
-'''
-# latex generator
-from pylatex import Document, Section, Subsection, Command, Figure
-from pylatex.utils import italic, NoEscape
-'''
-# text summarization
+#regular expression
+import re
+#convert latex formate text to pure text which has no command and structures.
+from pylatexenc.latex2text import LatexNodes2Text
+# text summarization with nltk
 import nltk
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -24,17 +19,13 @@ from nltk.corpus import stopwords
 #generate ppt pdf from latex
 from pdflatex import PDFLaTeX
 
-nltk.download('stopwords')
-
-HANDICAP = 0.85
-
 # extract paper latex from zip
 zip = "Bias_Final.zip"
 z = zipfile.ZipFile(zip, "r")
 z.extractall()
 z.close()
 
-# remove latex comment
+# remove latex comment %
 tx = tx_process = 'AAAI-SenP.1698.tex'
 f = open(tx, 'r')
 a = f.readlines()
@@ -45,6 +36,36 @@ for i in a:
     else:
         f.write(i)
 f.close()
+
+''''' not working remove {comment} section
+# use TexSoup to remove the {comment} section
+with open(tx_process) as f:
+    lat_repr = TexSoup(f)
+lat_cmts = lat_repr.find_all('comment')
+for cmt in lat_cmts:
+    lat_repr.remove(cmt)
+f = open(tx, 'w')
+f.write(str(lat_repr))
+f.close()
+'''''
+
+#regular experession with re package
+  # detect main text starting pattern
+MAIN_TEX_PATT = re.compile(r'(\\begin\s*\{\s*document\s*\})', re.I)
+  # detect end pattern
+TEX_END_PATT = re.compile(r'(\\end\s*\{\s*document\s*\})', re.I)
+  # detect section{}, subsection{}, subsubsection{}, paragraph{}, subparagraph{}
+SEC_PATT = re.compile(r'\\section\s*\{[^\n]+?\}', re.I)   # \\->\, \s->any space,[^\n]->no \n,+?->at least one thing existing, \}->}
+PARA_PATT = re.compile(r'\\paragraph\s*\{[^\n]+?\}', re.I)
+SUBPARA_PATT = re.compile(r'\\subparagraph\s*\{[^\n]+?\}', re.I)
+SUBSEC_PATT = re.compile(r'\\subsection\s*\{[^\n]+?\}', re.I)
+SUBSUBSEC_PATT = re.compile(r'\\subsubsection\s*\{[^\n]+?\}', re.I)
+
+#store whole paper latex string in paper_latex
+paper_latex = open(tx).read()
+print("--------------------------------read paper latex-----------------------\n")
+#print(paper_latex)
+print("--------------------------------read paper latex end-----------------------\n")
 
 # make TexSoup object to do parsing
 with open(tx_process) as f:
@@ -126,8 +147,20 @@ for i in sec_list:
 
 print("\n--------------------------slice paper section end-------------------------------------------\n")
 
+# removing formatting commands in a latex string, thus converting it into a pure text
+def latex2text(lat_string):
+    """
+    :param lat_string:
+    :return: string
+    """
+    return LatexNodes2Text().latex_to_text(lat_string)
 
-# ai text summarization
+
+# ai text summarization with nltk
+nltk.download('stopwords')
+
+HANDICAP = 0.5
+
 def remove_punctuation_marks(text):
     punctuation_marks = dict((ord(punctuation_mark), None) for punctuation_mark in string.punctuation)
     return text.translate(punctuation_marks)
@@ -188,7 +221,9 @@ tfidf_results = TfidfVectorizer(tokenizer=get_lemmatized_tokens,
 print("\n--------------------------summary of all---------------------------------------------------\n")
 #print(get_summary(documents, tfidf_results))
 print("\n--------------------------summary end---------------------------------------------------------\n")
+##### ai text summarization with nltk end #####
 
+#format text content into beamer ppt latex
 def add_text_frame(text_list, frame_name, beamer_file):
 
     beamer_file.writelines(r'\begin{}'.format('{frame}' + '{' + frame_name + '}') + '\n')
@@ -201,6 +236,7 @@ def add_text_frame(text_list, frame_name, beamer_file):
     beamer_file.writelines(r'\end{frame}' + '\n')
     beamer_file.writelines(r'%' + '\n')
 
+#format figure content into beamer ppt latex
 def add_figure_frame(fig_str, frame_name, beamer_file):
 
     for fig in fig_str:
@@ -211,6 +247,7 @@ def add_figure_frame(fig_str, frame_name, beamer_file):
         beamer_file.writelines(r'\end{frame}' + '\n')
         beamer_file.writelines(r'%' + '\n')
 
+#format table content into beamer ppt latex
 def add_table_frame(table_str, frame_name, beamer_file):
 
     for table in table_str:
@@ -221,6 +258,7 @@ def add_table_frame(table_str, frame_name, beamer_file):
         beamer_file.writelines(r'\end{frame}' + '\n')
         beamer_file.writelines(r'%' + '\n')
 
+#format equation content into beamer ppt latex
 def add_equation_frame(equation_str, frame_name, beamer_file):
     for equation in equation_str:
         beamer_file.writelines(r'\begin{}'.format('{frame}' + '{' + frame_name + '}') + '\n')
@@ -230,6 +268,29 @@ def add_equation_frame(equation_str, frame_name, beamer_file):
         beamer_file.writelines(r'\end{frame}' + '\n')
         beamer_file.writelines(r'%' + '\n')
 
+
+#for a smallest string level section, extract text content latex
+def extract_text_content(lat_string, cnt=('figure', 'table', 'equation')):
+    """
+    :param lat_string:
+    :param cnt: contents to extract, iterable
+    :return: dict
+    """
+    res = {}
+    # convert to TexSoup
+    lat_repr = TexSoup(lat_string)
+    for cnt_type in cnt:
+        items = lat_repr.find_all(cnt_type)
+        # remove found items, so only pure text remains
+        for item in items:
+            lat_repr.remove(item)
+        res[cnt_type] = items
+    # also save the pure text
+    res['text'] = str(lat_repr)
+    return res['text']
+
+
+#transfer the whole paper latex into ppt beamber latex
 with open('presentation_test.tex', 'w') as tex_f:
     # headers and packages
     tex_f.writelines(r'\documentclass{beamer}' + '\n')
@@ -238,6 +299,7 @@ with open('presentation_test.tex', 'w') as tex_f:
     tex_f.writelines(r'\usepackage{lmodern}' + '\n')
     tex_f.writelines(r'\usepackage{textcomp}' + '\n')
     tex_f.writelines(r'\usepackage{lastpage}' + '\n')
+    tex_f.writelines(r'\usepackage{adjustbox}' + '\n')
     tex_f.writelines(r'%' + '\n')
     tex_f.writelines(r'\title{}'.format('{' + slide_title + '}') + '\n')
     tex_f.writelines(r'%' + '\n')
@@ -246,7 +308,7 @@ with open('presentation_test.tex', 'w') as tex_f:
     tex_f.writelines(r'\maketitle' + '\n')
     tex_f.writelines(r'%' + '\n')
 
- #  add_text_frame(section_summary, 'Introduction', tex_f)
+    #section by section
     for section_sample in sec_list:
         # one section decomposition
         print("\n--------------------------see a section---------------------------------------------------\n")
@@ -254,13 +316,23 @@ with open('presentation_test.tex', 'w') as tex_f:
         print(
             "\n--------------------------see a section end---------------------------------------------------------\n")
         print("\n--------------------------summarize a section--------------------------------------------\n")
-        documents = nltk.sent_tokenize(section_sample)
+        sec_text = extract_text_content(section_sample, cnt=('figure', 'table', 'equation'))
+        sec_text=latex2text(sec_text)
+        documents = nltk.sent_tokenize(sec_text)
         tfidf_results = TfidfVectorizer(tokenizer=get_lemmatized_tokens,
                                         stop_words=stopwords.words('english')).fit_transform(documents)
         section_summary = get_summary(documents, tfidf_results).split('\n')
+        better_section_summary = []
+        print(section_summary)
+        for i in section_summary:
+            if i != '' and i !=' ' and i != '  ' and i!='   ' and i!='    ' and i!='     ' and i!='      ':
+                better_section_summary.append(i)
+        for i in better_section_summary:
+            print(i)
+        print(better_section_summary)
         print("\n--------------------------summarize a section end----------------------------------------\n")
         soup_section = TexSoup(section_sample)
-        print(soup_section)
+        #print(soup_section)
         print("--------------------------------retrieve section title-----------------------\n")
         a_sec_title = soup_section.section.string
         print(a_sec_title)
@@ -278,7 +350,9 @@ with open('presentation_test.tex', 'w') as tex_f:
         a_sec_equations = soup_section.find_all('equation')
         print(a_sec_equations)
         print("--------------------------------retrieve section equations end-----------------------\n")
-        add_text_frame(['abd','def','ghi','jkl'], a_sec_title, tex_f)
+
+        #one section composition
+        add_text_frame(better_section_summary, a_sec_title, tex_f)
         add_figure_frame(a_sec_figures, a_sec_title, tex_f)
         add_table_frame(a_sec_tables,a_sec_title,tex_f)
         add_equation_frame(a_sec_equations, a_sec_title, tex_f)
@@ -286,5 +360,33 @@ with open('presentation_test.tex', 'w') as tex_f:
     tex_f.writelines(r'\end{document}' + '\n')
 
 print(sec_title_list)
+
+print("--------------------------------a section table experiment -----------------------\n")
+print(a_sec_tables)
+print("--------------------------------a section table experiment end-----------------------\n")
+
+
+
+#for a smallest string level section, decomposite it into dict,xtract equations, figures and tables from a latex string
+def extract_latex_content(lat_string, cnt=('figure', 'table', 'equation')):
+    """
+    :param lat_string:
+    :param cnt: contents to extract, iterable
+    :return: dict
+    """
+    res = {}
+    # convert to TexSoup
+    lat_repr = TexSoup(lat_string)
+    for cnt_type in cnt:
+        items = lat_repr.find_all(cnt_type)
+        # remove found items, so only pure text remains
+        for item in items:
+            lat_repr.remove(item)
+        res[cnt_type] = items
+    # also save the pure text
+    res['text'] = str(lat_repr)
+    return res
+
+
 
 
